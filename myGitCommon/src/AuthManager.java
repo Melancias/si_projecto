6a593dfe17a3e91f5a21851ea97803067b55a43a
pedro <pedro.alex.candido@gmail.com>
2017-03-08 12:53:17 +0000

    Added AuthManager: registers, authenticates, creates .authFile

--- myGitCommon/src/AuthManager.java
+++ myGitCommon/src/AuthManager.java
@@ -1,11 +1,62 @@
+import java.io.*;
+
 /**
  * Created by Melancias on 03/03/2017.
  */
 
 public class AuthManager {
 
-    public AuthManager() {
+    private File authFile;
 
+    public AuthManager() {
+        this.authFile = new File("./.authFile");
+    }
+
+
+    public boolean authenticate(String username, String password){
+
+        try {
+            BufferedReader authReader = new BufferedReader(new FileReader(authFile));
+
+            // Read each line until credentials match
+            for (String line; (line = authReader.readLine()) != null;){
+                String[] credentials = line.split("\\:");
+                if (credentials[0].equals(username) && credentials[1].equals(password)) {
+                    return true;
+                }
+            }
+            // In case credentials not found, register
+            return register(username, password);
+
+        } catch (FileNotFoundException e) {
+            createAuthFile();
+            register(username, password);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+
+    }
+
+    private boolean register(String username, String password){
+        try {
+            FileWriter authWriter = new FileWriter(authFile, true);
+            String credentials = username+":"+password;
+            authWriter.append(credentials);
+        } catch (IOException e) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean createAuthFile(){
+        try {
+            return authFile.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
 }
---
+++ myGitCommon/src/ShareManager.java
@@ -0,0 +1,12 @@
+import sun.security.provider.SHA;
+
+/**
+ * Created by pedro on 08-03-2017.
+ */
+public class ShareManager {
+
+    public ShareManager(){
+
+    }
+
+}
