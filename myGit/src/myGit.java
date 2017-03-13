ebe90528665ac354f0b491044c2af8d16b165bfb
fc45786 <fc45786@alunos.fc.ul.pt>
2017-03-13 00:38:31 +0000

    Cliente modificado

--- myGit/src/myGit.java
+++ myGit/src/myGit.java
@@ -12,15 +12,26 @@ public class myGit {
     public static void main (String[] args) throws IOException, ClassNotFoundException {
         String argumento =args[0];
         if(argumento.equals("-init")){
-            Path currentRelativePath = Paths.get("");
-            String s = currentRelativePath.toAbsolutePath().toString();
-            System.out.println(s);
-            File f = new File(s + "\\"+ args[1]);
-            if(!f.exists()){
-                f.mkdir();
+            try{
+                Path currentRelativePath = Paths.get("");
+                String s = currentRelativePath.toAbsolutePath().toString();
+                System.out.println(s);
+                File f = new File(s + "\\"+ args[1]);
+                if(f.isDirectory()) {
+                    if (!f.exists()) {
+                        f.mkdir();
+                        System.out.println("O repositorio " + args[1] + "  foi criado localmente");
+                    } else
+                        System.out.println("O repositorio ja existe");
+                }
+
+            }catch (Exception e){
+                System.out.println("Nao foi possivel criar o repositorio");
             }
+
         }
         else{
+
             String[] address;
             String host = null;
             int port = 0;
@@ -36,17 +47,15 @@ public class myGit {
                 }
                 DataTransferUtils util = null;
                 util = new DataTransferUtils(host, port, localUser);
+                if(args.length < 5){
 
+                    System.exit(0);
+                }else{
                 if (!util.authClient(argumento, args[3])) {
                     // util.createUser(argumento, args[3]);
                     System.out.println("Authentication failed");
                     System.exit(-1);
                 }
-
-            if(args.length < 5){
-
-                System.exit(0);
-            } else {
                 if(args[4].equals("-push")){
                     Path currentRelativePath = Paths.get("");
                     String s = currentRelativePath.toAbsolutePath().toString();
@@ -72,19 +81,39 @@ public class myGit {
                         // TODO: Olá empregador que esta a ver o meu codigo no github para ver se eu sou um bom candidato
                         // TODO: Eu sou assiduo, pontual e com um espirito muito trabalhador.
                         // TODO: Não me importo de fazer horas extra de borla ;). mesmo que nao sejam extra
-                        util.pullFile(f);
-                        System.out.println("O ficheiro foi copiado do servidor");
+                        try{
+                            util.pullFile(s + "/" + args[5]);
+                            System.out.println("O ficheiro " + args[5] + " foi copiado do servidor");
+                        }catch (Exception e){
+                            System.out.println("Nao foi possivel copiar o ficheiro " + args[5] + "do servidor");
+                        }
+
                     }else if(f.isDirectory()){
-                        for(File file: f.listFiles())
-                            util.pullFile(file);
-                        System.out.println("O repositorio foi copiado do servidor");
+                        try{
+                            for(File file: f.listFiles())
+                                util.pullFile(s + "/" + args[5] + "/" + file.getPath());
+                            System.out.println("O repositorio " + args[5] + " foi copiado do servidor");
+                        }catch (Exception e){
+                            System.out.println("Nao foi possivel copiar o repositorio" + args[5] + " do servidor");
+                        }
+
                     }
 
 
                 }else if(args[4].equals("-share")){
-                    util.share("share", argumento, args[5], args[6]);
+                    try{
+                        util.share("share", argumento, args[5], args[6]);
+                    }catch (Exception e){
+                        System.out.println("Ocorreu um erro");
+                    }
+
                 }else if(args[4].equals("remove"))
-                    util.remove("remove", argumento, args[5], args[6]);
+                    try{
+                        util.remove("remove", argumento, args[5], args[6]);
+                    }catch (Exception e){
+                        System.out.println("Ocorreu um erro");
+                    }
+
             }
         }
     }
